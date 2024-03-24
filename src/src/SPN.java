8cca2ae496ddcd23674e8d5268789b259d86f72e
Hava Fuga <hava_fuga@outlook.com>
2024-03-24 15:46:01 +0100

    refactor: push for tracking (not functional code)

--- src/src/SPN.java
+++ src/src/SPN.java
@@ -1,8 +1,13 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 public class SPN {
     private int r = 4;
     private int n = 4;
     private int m = 4;
     private int s = 32;
+    private int l = 16;
     private String[] keys = new String[r+1];
     //private ArrayList x = new ArrayList();
     private char[] sbox = {'E', '4', 'D', '1', '2', 'F', 'B', '8', '3', 'A', '6', 'C', '5', '9', '0', '7'};
@@ -15,34 +20,61 @@ public class SPN {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        new SPN("0001 0001 0010 1000 1000 1100 0000 0000", "HELLo");
+        new SPN("HELLo", "00010001001010001000110000000000");
 
 
 
     }
 
 
-    public SPN (String key, String text) {
-        // Dabei wird ein Text zunächt ASCII kodiert
-        //createBitstring(text);
-        snp("0001001010001111", "00010001001010001000110000000000");
+    public SPN (String text, String key) {
+        // snp("0001001010001111", "00010001001010001000110000000000"); // test
 
 
-
+        // decrypt(createBitstring(text), key);
+        decrypt("00000100110100100000101110111000000000101000111110001110011111110110000001010001010000111010000000010011011001110010101110110000", key);
     }
 
-    protected void snp(String x, String key) {
+    private void decrypt(String chiffretext, String key) {
+        String text = "";
 
+        String random = generateRandomBitString();
 
+        System.out.println("random " + random);
+
+        for (int i = 0; i < chiffretext.length(); i+= l) {
+            String block = chiffretext.substring(i, Math.min(i + l, chiffretext.length()));
+            String bitAddition = Integer.toBinaryString(Integer.parseInt(random, 2) + i / l);
+
+            while (bitAddition.length() < l) {
+                bitAddition = "0" + bitAddition;
+            }
+
+            String encryptedBlock = xor(snp(bitAddition, key), block);
+            text += encryptedBlock;
+        }
+
+        System.out.println("text " + text);
+    }
+
+    // help https://docs.oracle.com/javase/7/docs/api/java/util/Random.html#nextInt%28int%29
+    private String generateRandomBitString() {
+        int randomNumber = new Random().nextInt((int) (Math.pow(2, l)-1));
+        String binaryString = Integer.toBinaryString(randomNumber);
+        while (binaryString.length() < l) {
+            binaryString = "0" + binaryString; // erweitere dass binary gleiche Länge
+        }
+        return binaryString;
+    }
+
+    private String snp(String x, String key) {
         // berechne Rundenschlüssel
         calculateK(key);
-        System.out.println("keys.length " + keys.length);
         this.x = x;
 
 
         // 1. weisschritt
         xor(x, keys[0]);
-        System.out.println("keys[0] " + keys[0]);
 
         for (int i = 0; i < r-1; i++ ) {
             // 2.
@@ -53,22 +85,19 @@ public class SPN {
             bitpermutation();
 
             // c. rundschlüssel();
-            xor(this.x, keys[i+1]);
-            System.out.println("keys[i+1] " + keys[i+1]);
+            xor(x, keys[i+1]);
         }
 
         // 3. letzte Runde
         // a.
         sbox();
         // c. rundschlüssel();
-        System.out.println("keys[r] " + keys[r-1]);
-        xor(this.x, keys[r]);
+        xor(x, keys[r]);
 
-        System.out.println(this.x);
-        return;
+        return this.x;
     }
 
-    private void createBitstring(String text) {
+    private String createBitstring(String text) {
         String bitString = "";
 
         for (int i = 0; i < text.length(); i++) {
@@ -89,19 +118,14 @@ public class SPN {
         while (bitString.length() % 16 != 0) {
             bitString = "0" + bitString;
         }
-    }
 
-    public void dechiffre() {
-        // berechne K'
-
-
+        return bitString;
     }
 
     private void calculateK(String key) {
         // K(k, i) bestehe aus den 16 aufeinanderfolgenden Bits von k beginnend bei Position 4i
         for (int i = 0; i < r+1; i++) {
             keys[i] = key.substring(4*i, 4*i + 16);
-            System.out.println("key.substring(4*i, 4*i + 16) " + key.substring(4*i, 4*i + 16));
         }
     }
 
@@ -122,7 +146,7 @@ public class SPN {
 
             String binaryValue = Integer.toBinaryString(Character.digit(sbox[Integer.parseInt(substring, 2)], 16));
             while (binaryValue.length() < 4) {
-                binaryValue = "0" + binaryValue; // Ensure each binary string has length 4
+                binaryValue = "0" + binaryValue; // erweitere dass binary gleiche Länge
             }
 
             result += binaryValue;
@@ -130,13 +154,6 @@ public class SPN {
         x = result;
     }
 
-    private void weisschritt() {
-    }
-
-    /**
-     * help from
-     * https://stackoverflow.com/questions/5126616/xor-operation-with-two-strings-in-java
-     */
     private String xor(String a, String b)  {
         if (a.length() != b.length()) throw new RuntimeException("XOR a und b sind nicht gleichlang");
 
